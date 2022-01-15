d3b99a075b28d04d9cd704b5094e6445122788d3
greenjoe071 <84485320+greenjoe071@users.noreply.github.com>
2022-01-15 10:47:48 -0600

    Add files via upload

---
+++ photo-app-frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app_header">
+        <img className="app_headerImage" src="camera_icon.png" alt="header" />
+      </div>
+      <h1>Photo Sharing App</h1>
+        
+    </div>
+  );
+}
+
+export default App;
